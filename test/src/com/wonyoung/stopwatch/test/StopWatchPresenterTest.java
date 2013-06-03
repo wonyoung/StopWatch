3c11c2db98178adede47d337003165efd4168ae3
Wonyoung <wonyoungjjang@gmail.com>
2013-06-03 09:09:21 +0900

    first commit

---
+++ test/src/com/wonyoung/stopwatch/test/StopWatchPresenterTest.java
@@ -0,0 +1,14 @@
+package com.wonyoung.stopwatch.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StopWatchPresenterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
